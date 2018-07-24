0b8809a103e91cc407b6e7d57b8d114a1749270f
helo <hello>
2018-07-24 09:43:56 +0800

    优化网络

--- android-common8/src/main/java/android/skin/SkinUtils.java
+++ android-common8/src/main/java/android/skin/SkinUtils.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package android.skin;
 
@@ -9,37 +9,40 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.utils.ShowLog;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 /**
- *****************************************************************************************************************************************************************************
- * 设置 颜色 背景 图片 
+ * ****************************************************************************************************************************************************************************
+ * 设置 颜色 背景 图片
+ *
  * @author :Atar
  * @createTime:2017-9-18上午11:38:59
  * @version:1.0.0
  * @modifyTime:
  * @modifyAuthor:
- * @description:
- *****************************************************************************************************************************************************************************
+ * @description: ****************************************************************************************************************************************************************************
  */
 @SuppressLint("Recycle")
 public class SkinUtils {
 
 	/**
 	 * 设置背景
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param skinType
+	 * @param views
 	 * @author :Atar
 	 * @createTime:2017-9-18上午11:45:29
 	 * @version:1.0.0
 	 * @modifyTime:
 	 * @modifyAuthor:
-	 * @param context
-	 * @param resources 
-	 * @param resourcesName 
-	 * @param skinType
-	 * @param views
 	 * @description:
 	 */
 	public static void setBackgroundColor(Context context, int resourcesName, int skinType, View... views) {
@@ -57,16 +60,16 @@ public class SkinUtils {
 
 	/**
 	 * 设置字体颜色
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param skinType
+	 * @param textView
 	 * @author :Atar
 	 * @createTime:2017-9-18上午11:45:53
 	 * @version:1.0.0
 	 * @modifyTime:
 	 * @modifyAuthor:
-	 * @param context
-	 * @param resources
-	 * @param resourcesName
-	 * @param skinType
-	 * @param textView
 	 * @description:
 	 */
 	public static void setTextColor(Context context, int resourcesName, int skinType, TextView... textView) {
@@ -84,16 +87,16 @@ public class SkinUtils {
 
 	/**
 	 * 设置背景
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param skinType
+	 * @param views
 	 * @author :Atar
 	 * @createTime:2017-9-18上午11:45:29
 	 * @version:1.0.0
 	 * @modifyTime:
 	 * @modifyAuthor:
-	 * @param context
-	 * @param resources 
-	 * @param resourcesName 
-	 * @param skinType
-	 * @param views
 	 * @description:
 	 */
 	@SuppressWarnings("deprecation")
@@ -112,16 +115,15 @@ public class SkinUtils {
 
 	/**
 	 * 设置图片
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param skinType
 	 * @author :Atar
 	 * @createTime:2017-9-18上午11:45:29
 	 * @version:1.0.0
 	 * @modifyTime:
 	 * @modifyAuthor:
-	 * @param context
-	 * @param resources 
-	 * @param resourcesName 
-	 * @param skinType
-	 * @param views
 	 * @description:
 	 */
 	public static void setImageDrawable(Context context, int resourcesName, int skinType, ImageView... imageViews) {
@@ -139,16 +141,16 @@ public class SkinUtils {
 
 	/**
 	 * 设置listView分隔线
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param skinType
+	 * @param listView
 	 * @author :Atar
 	 * @createTime:2017-9-18上午11:47:01
 	 * @version:1.0.0
 	 * @modifyTime:
 	 * @modifyAuthor:
-	 * @param context
-	 * @param resources
-	 * @param resourcesName
-	 * @param skinType
-	 * @param listView
 	 * @description:
 	 */
 	public static void setDivider(Context context, int resourcesName, int skinType, ListView listView) {
@@ -160,16 +162,17 @@ public class SkinUtils {
 
 	/**
 	 * 设置Array下文字
-	 * @author :Atar
-	 * @createTime:2017-9-19下午4:18:28
-	 * @version:1.0.0
-	 * @modifyTime:
-	 * @modifyAuthor:
+	 *
 	 * @param context
 	 * @param resources
 	 * @param resourcesName
 	 * @param skinType
 	 * @param textView
+	 * @author :Atar
+	 * @createTime:2017-9-19下午4:18:28
+	 * @version:1.0.0
+	 * @modifyTime:
+	 * @modifyAuthor:
 	 * @description:
 	 */
 	public static void setText(Context context, Resources resources, int resourcesName, int skinType, TextView... textView) {
@@ -187,17 +190,16 @@ public class SkinUtils {
 
 	/**
 	 * 得到Array下颜色值
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param skinType
+	 * @return
 	 * @author :Atar
 	 * @createTime:2017-9-19下午2:53:24
 	 * @version:1.0.0
 	 * @modifyTime:
 	 * @modifyAuthor:
-	 * @param context
-	 * @param resources
-	 * @param resourcesName
-	 * @param skinType
-	 * @param listView
-	 * @return
 	 * @description:
 	 */
 	public static int getArrayColor(Context context, int resourcesName, int skinType) {
@@ -212,16 +214,16 @@ public class SkinUtils {
 
 	/**
 	 * 得到Array下drawable
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param skinType
+	 * @return
 	 * @author :Atar
 	 * @createTime:2017-9-19下午2:55:55
 	 * @version:1.0.0
 	 * @modifyTime:
 	 * @modifyAuthor:
-	 * @param context
-	 * @param resources
-	 * @param resourcesName
-	 * @param skinType
-	 * @return
 	 * @description:
 	 */
 	public static Drawable getArrayDrawable(Context context, int resourcesName, int skinType) {
@@ -236,16 +238,16 @@ public class SkinUtils {
 
 	/**
 	 * 得到Array下String
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param skinType
+	 * @return
 	 * @author :Atar
 	 * @createTime:2017-9-19下午4:14:54
 	 * @version:1.0.0
 	 * @modifyTime:
 	 * @modifyAuthor:
-	 * @param context
-	 * @param resources
-	 * @param resourcesName
-	 * @param skinType
-	 * @return
 	 * @description:
 	 */
 	public static String getArrayString(Context context, int resourcesName, int skinType) {
@@ -259,15 +261,15 @@ public class SkinUtils {
 
 	/**
 	 * 得到颜色
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @return
 	 * @author :Atar
 	 * @createTime:2017-9-19下午4:26:01
 	 * @version:1.0.0
 	 * @modifyTime:
 	 * @modifyAuthor:
-	 * @param context
-	 * @param resources
-	 * @param resourcesName
-	 * @return
 	 * @description:
 	 */
 	public static int getColor(Context context, int resourcesName) {
@@ -329,6 +331,191 @@ public class SkinUtils {
 			return resources.getDimension(resources.getIdentifier(context.getResources().getString(resourcesName), "dimen", SkinResourcesManager.getInstance(context).getSkinPackName()));
 		} catch (Exception e) {
 			return 0;
+		}
+	}
+
+	/**
+	 * 设置text文字
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param textView
+	 */
+	public static void setText(Context context, int resourcesName, TextView... textView) {
+		try {
+			if (textView != null && context != null && textView.length > 0) {
+				for (int i = 0; i < textView.length; i++) {
+					textView[i].setText(getString(context, resourcesName));
+				}
+			}
+		} catch (Exception e) {
+		}
+	}
+
+	/**
+	 * 设置图片
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param imageView
+	 */
+	public static void setImageDrawable(Context context, int resourcesName, ImageView... imageView) {
+		try {
+			if (imageView != null && context != null && imageView.length > 0) {
+				for (int i = 0; i < imageView.length; i++) {
+					imageView[i].setImageDrawable(getDrawable(context, resourcesName));
+				}
+			}
+		} catch (Exception e) {
+		}
+	}
+
+	/**
+	 * 设置背景图片
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param view
+	 */
+	public static void setBackgroundDrawable(Context context, int resourcesName, View... view) {
+		try {
+			if (view != null && context != null && view.length > 0) {
+				for (int i = 0; i < view.length; i++) {
+					view[i].setBackgroundDrawable(getDrawable(context, resourcesName));
+				}
+			}
+		} catch (Exception e) {
+		}
+	}
+
+	/**
+	 * 设置EditText hint文字
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param editTexts
+	 */
+	public static void setHint(Context context, int resourcesName, EditText... editTexts) {
+		try {
+			if (editTexts != null && context != null && editTexts.length > 0) {
+				for (int i = 0; i < editTexts.length; i++) {
+					editTexts[i].setHint(getString(context, resourcesName));
+				}
+			}
+		} catch (Exception e) {
+		}
+	}
+
+	/**
+	 * 设置背景
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param views
+	 * @author :Atar
+	 * @createTime:2017-9-18上午11:45:29
+	 * @version:1.0.0
+	 * @modifyTime:
+	 * @modifyAuthor:
+	 * @description:
+	 */
+	public static void setBackgroundColor(Context context, int resourcesName, View... views) {
+		if (context != null) {
+			if (views != null && views.length > 0) {
+				for (int i = 0; i < views.length; i++) {
+					try {
+						views[i].setBackgroundColor(getColor(context, resourcesName));
+					} catch (Exception e) {
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * 设置字体颜色
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param textView
+	 * @author :Atar
+	 * @createTime:2017-9-18上午11:45:53
+	 * @version:1.0.0
+	 * @modifyTime:
+	 * @modifyAuthor:
+	 * @description:
+	 */
+	public static void setTextColor(Context context, int resourcesName, TextView... textView) {
+		if (context != null) {
+			if (textView != null && textView.length > 0) {
+				for (int i = 0; i < textView.length; i++) {
+					try {
+						textView[i].setTextColor(getColor(context, resourcesName));
+					} catch (Exception e) {
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * 设置EditText hintColor
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param editText
+	 */
+	public static void setHintTextColor(Context context, int resourcesName, EditText... editText) {
+		if (context != null) {
+			if (editText != null && editText.length > 0) {
+				for (int i = 0; i < editText.length; i++) {
+					try {
+						editText[i].setHintTextColor(getColor(context, resourcesName));
+					} catch (Exception e) {
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * 设置EditText hintColor
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param editText
+	 */
+	public static void setHintTextColor(Context context, int resourcesName, int skinType, EditText... editText) {
+		if (context != null) {
+			if (editText != null && editText.length > 0) {
+				for (int i = 0; i < editText.length; i++) {
+					try {
+						editText[i].setHintTextColor(getArrayColor(context, resourcesName, skinType));
+					} catch (Exception e) {
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * CompoundButton 设置ButtonDrawable
+	 *
+	 * @param context
+	 * @param resourcesName
+	 * @param skinType
+	 * @param compoundButton
+	 */
+	public static void setButtonDrawable(Context context, int resourcesName, int skinType, CompoundButton... compoundButton) {
+		if (context != null) {
+			if (compoundButton != null && compoundButton.length > 0) {
+				for (int i = 0; i < compoundButton.length; i++) {
+					try {
+						compoundButton[i].setButtonDrawable(getArrayDrawable(context, resourcesName, skinType));
+					} catch (Exception e) {
+					}
+				}
+			}
 		}
 	}
 }
